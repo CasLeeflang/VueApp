c704ccb7e1e890085f40fa2f026d6e72dcde6f29
casleeflang <466933@student.fontys.nl>
2022-06-21 13:21:24 +0200

    iniy

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg }} Cookie: {{ this.username }}</h1>
+    <button v-on:click="getCookie()">Click</button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -8,8 +9,10 @@
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank"
+             rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank"
+             rel="noopener">eslint</a></li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
@@ -23,7 +26,8 @@
     <ul>
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
       <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a>
+      </li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
@@ -31,11 +35,23 @@
 </template>
 
 <script>
+import VueJwtDecode from 'vue-jwt-decode'
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
+    msg: String,
+  },
+  data() {
+    return {
+      username: ""
+    }
+  },
+  created() {
+    var cook = localStorage.getItem("adal.idtoken");
+    var decoded = VueJwtDecode.decode(cook);
+    // eslint-disable-next-line vue/no-mutating-props,vue/no-side-effects-in-computed-properties
+    this.username = decoded['name']
+  },
 }
 </script>
 
@@ -44,14 +60,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import Adal from 'vue-adal'
+import VueJwtDecode from 'vue-jwt-decode'
+
 
 Vue.config.productionTip = false
+Vue.use(VueJwtDecode)
+
+Vue.use(Adal, {
+  config: {
+    tenant: 'e5f13437-7ed2-4d8e-9e3c-cac8cd067f51',
+    clientId: 'fbfc966b-b491-44f7-9a05-17aac9ce422f',
+    // Change depending on where it is hosted
+    redirectUri: 'http://localhost:3000',
+
+    cacheLocation: 'localStorage'
+  },
+
+  requireAuthOnInitialize: true,
+  router: router
+
+})
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
